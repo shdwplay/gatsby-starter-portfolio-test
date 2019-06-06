9b6faf6b283377a69c170e4ff8c4a6f8f47bed47
Lorenzo <lrnzcbc@gmail.com>
2019-06-06 20:12:31 +0200

    information change

--- config/site.js
+++ config/site.js
@@ -1,21 +1,21 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
-  siteTitle: 'Emilia', // Navigation and Site Title
-  siteTitleAlt: 'Emilia - Gatsby Starter Portfolio', // Alternative Site title for SEO
-  siteTitleShort: 'Emilia', // short_name for manifest
-  siteHeadline: 'Publishing & Creating stunning photos', // Headline for schema.org JSONLD
-  siteUrl: 'https://emilia.lekoarts.de', // Domain of your site. No trailing slash!
+  siteTitle: 'Della Badia', // Navigation and Site Title
+  siteTitleAlt: 'Anna Della Badia Portfolio', // Alternative Site title for SEO
+  siteTitleShort: 'ANNA', // short_name for manifest
+  siteHeadline: 'Direzione della fotografia ecc', // Headline for schema.org JSONLD
+  siteUrl: '', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo.png', // Used for SEO and manifest
   siteDescription: 'Dark One-Page portfolio with cards & detailed project views',
   author: 'LekoArts', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@emilia', // Twitter Username
-  ogSiteName: 'emilia', // Facebook Site Name
-  ogLanguage: 'en_US', // og:language
-  googleAnalyticsID: 'UA-47519312-4',
+  // userTwitter: '@emilia', // Twitter Username
+  // ogSiteName: 'emilia', // Facebook Site Name
+  // ogLanguage: 'en_US', // og:language
+  // googleAnalyticsID: 'UA-47519312-4',
 
   // Manifest and Progress color
   themeColor: '#3498DB',
@@ -23,19 +23,19 @@ module.exports = {
 
   // Your information
   avatar: '/logos/lekoarts.png',
-  name: 'LekoArts',
-  location: 'Germany',
+  name: 'Anna Della Badia',
+  location: 'Italy',
   socialMedia: [
     {
-      url: 'https://twitter.com/lekoarts_de',
+      url: '',
       name: 'Twitter',
     },
     {
-      url: 'https://www.instagram.com/lekoarts.de',
+      url: 'https://www.instagram.com/annadellabadia/',
       name: 'Instagram',
     },
     {
-      url: 'https://www.lekoarts.de/en',
+      url: '',
       name: 'Homepage',
     },
   ],
